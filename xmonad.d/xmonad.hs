a02eeff6eaafe0b6659205d636515f3b66201dbb
l6y <mail.address.is.unsteady@gmail.com>
2013-05-13 12:48:41 +0900

    Added configuration files

---
+++ xmonad.d/xmonad.hs
@@ -0,0 +1,16 @@
+import XMonad
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+
+main = do
+  xmproc <- spawnPipe "xmobar"
+  xmonad =<< xmobar defaultConfig
+    {
+      terminal = "urxvt",
+      normalBorderColor = "#839496",
+      focusedBorderColor = "#6c71c4",
+      manageHook = manageDocks <+> manageHook defaultConfig,
+      layoutHook = avoidStruts  $  layoutHook defaultConfig,
+      modMask = mod4Mask
+    }
