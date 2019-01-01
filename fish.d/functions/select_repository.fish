d2be4982c7bfc5b05d741d7d34ee20b6f6ad1304
u8y <mail.address.is.unsteady@gmail.com>
2019-01-02 05:19:57 +0900

    fixed fzf parameter

--- fish.d/functions/select_repository.fish
+++ fish.d/functions/select_repository.fish
@@ -1,5 +1,5 @@
 function select_repository --description "Change current directory to repository\'s root that managed by ghq"
-    ghq list -p | fzf --select-1 | read -l line && builtin cd $line
+    ghq list -p | fzf --layout=reverse --select-1 | read -l line && builtin cd $line
 
     commandline -f repaint
 end
