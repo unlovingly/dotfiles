95c4238ea0ec89bbdb4db2f4f48fe715e338f9ce
u8y <mail.address.is.unsteady@gmail.com>
2019-01-02 19:39:21 +0900

    updated prompt

--- fish.d/functions/fish_prompt.fish
+++ fish.d/functions/fish_prompt.fish
@@ -1,68 +1,59 @@
 function fish_prompt
+    set -l exit_code $status
     set -l git_info (fish_prompt_git)
     set -l vi_mode (fish_prompt_vi_mode)
 
-    set -l last_status $status
+    set -l name (whoami)
+    set -l mood " "(fish_prompt_status $exit_code)
+    set -l location " at "(set_color yellow)(prompt_hostname)(set_color $fish_color_normal)
+    set -l cwd " in "(set_color blue)(prompt_pwd)(set_color $fish_color_normal)
 
-    # TODO: shorten path if that too long
-    set -l basic_prompt (whoami)" at "(set_color yellow)(prompt_hostname)(set_color normal)" in "(set_color blue)(prompt_pwd)(set_color normal)
+    set -l basic_prompt $name$mood$location$cwd
 
-    set -l prompt $vi_mode
-
-    set -l status_info ""
-
-    if [ $last_status -ne 0 ]
-        set status_info (set_color red)"command failed with status: $last_status"(set_color normal)
-    end
-
-    if [ (id -u $USER) -eq 0 ]
-        set prompt (set_color red)"$prompt# "(set_color normal)
-    else
-        set prompt "$prompt% "
-    end
-
-    if [ -n $status_info ]
-        echo -s $status_info
-    end
+    set -l prompt $vi_mode" % "
 
     fish_prompt_print_first_line $basic_prompt $git_info
 
     echo -n -s $prompt
 end
 
-function fish_prompt_git
-    if [ (command git rev-parse --is-inside-work-tree ^/dev/null) ]
-        set -l result
-        set -l git_branch_name (command git symbolic-ref HEAD ^/dev/null | sed -e 's|^refs/heads/||')
-
-        if [ -z $git_branch_name ]
-            set git_branch_name (command git show-ref --head -s --abbrev | head -n1 2> /dev/null)
-        end
-
-        # Unconditional git component
-        set result "on "(set_color white)"$git_branch_name"
-
-        if [ (command git status -s --ignore-submodules=dirty | wc -l) -gt 0 ]
-            set result "$result"(set_color yellow)"*"
-        end
-
-        echo "$result"(set_color normal)
+function fish_prompt_status
+    if test $argv[1] -eq 0
+        echo (set_color green)":)"(set_color $fish_color_normal)
+    else
+        echo (set_color red)":("(set_color $fish_color_normal)
     end
 end
 
-function fish_prompt_print_first_line --description 'Prints first argument left-aligned, second argument right-aligned, newline'
-    if [ (count $argv) = 1 ]
+function fish_prompt_git
+    if test (command git rev-parse --is-inside-work-tree ^/dev/null)
+        set -l result
+        set -l git_branch_name (git symbolic-ref --short HEAD)
+
+        set result "on "(set_color white)"$git_branch_name"
+
+        if test (command git status -s --ignore-submodules=dirty | wc -l) -gt 0
+            set result "$result"(set_color yellow)"*"
+        end
+
+        echo "$result"(set_color $fish_color_normal)
+    end
+end
+
+function fish_prompt_print_first_line
+    if test (count $argv) -eq 1
         echo -s $argv
     else
         set -l left $argv[1]
         set -l right $argv[2]
-        # remove ANSI color
-        set -l arglength (string length (string replace -ra '\x1b.*?[mGKH]' '' "$left$right"))
-        set -l padding
 
-        if [ $arglength -lt $COLUMNS ]
-            # don't know the reason why `set_color normal` counted as the character
-            set padding (string repeat --count (math $COLUMNS - $arglength + 2 + 1) " ")
+        # remove ANSI color
+        set -l arglength (string length (string replace -ra "\x1b.*?[mGKH]" "" "$left$right"))
+        set -l padding (string repeat --count (math $COLUMNS - $arglength) " ")
+
+        if test $arglength -lt $COLUMNS
+            # I don't know the reason why `set_color $fish_color_normal` counted as the character
+            set padding $padding(string repeat --count 4 " ")
         end
 
         echo -s "$left$padding$right"
@@ -72,12 +63,12 @@ end
 function fish_prompt_vi_mode
     switch $fish_bind_mode
         case default
-            echo '[n] '
+            echo "[n]"
         case insert
-            echo '[i] '
+            echo "[i]"
         case visual
-            echo '[v] '
+            echo "[v]"
         case replace_one
-            echo '[r] '
+            echo "[r]"
     end
 end
