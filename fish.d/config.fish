1e95f13ec5218c230eb61ab8f9e9232f7d2828f7
u8y <mail.address.is.unsteady@gmail.com>
2019-06-15 22:07:56 +0900

    😭

--- fish.d/config.fish
+++ fish.d/config.fish
@@ -1,19 +1,10 @@
-set -g ANDROID_HOME /usr/local/opt/android-sdk
-set -g GOPATH ~/.local/go
-set -g JAVA_HOME /usr/libexec/java_home
-set -g NODE_PATH /usr/local/lib/node_modules
+set -g -x XDG_CONFIG_HOME ~/.config
 
-set -g fish_key_bindings rebind_keys
-
-set -g -x PATH /usr/local/bin $GOPATH/bin ~/.composer/vendor/bin ~/.cargo/bin $PATH
+sourcess $XDG_CONFIG_HOME/fish/aliases.fish
+sourcess $XDG_CONFIG_HOME/fish/env.fish
 
 # for HOMEBREW_GITHUB_API_TOKEN
-if set -q XDG_CONFIG_HOME
-    sourcess $XDG_CONFIG_HOME/fish/aliases.fish
-    sourcess $XDG_CONFIG_HOME/fish/env.fish
-    sourcess $XDG_CONFIG_HOME/fish/secret_env.fish
-end
+sourcess $XDG_CONFIG_HOME/fish/secret_env.fish
 
 
-status --is-interactive
-and sourcess (anyenv init -|psub)
+status --is-interactive && sourcess (anyenv init - | source)
--- fish.d/env.fish
+++ fish.d/env.fish
@@ -1,7 +1,13 @@
-#!/usr/bin/env fish
+set -g -x PATH /usr/local/bin $GOPATH/bin ~/.composer/vendor/bin ~/.cargo/bin $PATH
 set -g -x EDITOR code
 set -g -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
-set -g -x XDG_CONFIG_HOME ~/.config
+set -g -x LANG ja_JP.UTF-8
+
+set -g fish_key_bindings rebind_keys
+
+set -g GOPATH ~/.local/go
+set -g JAVA_HOME /usr/libexec/java_home
+set -g NODE_PATH /usr/local/lib/node_modules
 
 set -g __fish_git_prompt_show_informative_status 'yes'
 set -g __fish_git_prompt_showcolorhints 'yes'
