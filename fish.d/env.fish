3de5b6b0c48498614e59f0417da378bf86a23d32
u8y <mail.address.is.unsteady@gmail.com>
2020-06-01 00:54:55 +0900

    for wsl

--- fish.d/env.fish
+++ fish.d/env.fish
@@ -1,6 +1,9 @@
 set -g -x EDITOR nvim
 set -g -x FZF_DEFAULT_OPTS --exact --layout=reverse --height 15%
 set -g -x LANG ja_JP.UTF-8
+set -g -x LC_ALL ja_JP.UTF-8
+set -g -x LC_CTYPE ja_JP.UTF-8
+set -g -x LC_MESSAGES ja_JP.UTF-8
 set -g -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
 set -g -x GPG_TTY (tty)
 
