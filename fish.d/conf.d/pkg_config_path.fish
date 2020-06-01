14b792863b45eeb793dd71e37529888086e0e0ff
u8y <mail.address.is.unsteady@gmail.com>
2020-06-01 19:55:42 +0900

    ｳｯ

--- fish.d/conf.d/pkg_config_path.fish
+++ fish.d/conf.d/pkg_config_path.fish
@@ -3,7 +3,7 @@ if test -x brew
 
   set -g -x PHP_BUILD_CONFIGURE_OPTS "--enable-intl --with-zlib-dir=$pcp/opt/zlib --with-bz2=$pcp/opt/bzip2 --with-curl=$pcp/opt/openssl@1.1 --with-iconv=$pcp/opt/libiconv --with-tidy=$pcp/opt/tidy-html5"
   set -g -x PKG_CONFIG_PATH "$pcp/opt/krb5/lib/pkgconfig $pcp/opt/openssl/lib/pkgconfig $pcp/opt/icu4c/lib/pkgconfig $pcp/opt/libedit/lib/pkgconfig $pcp/opt/libxml2/lib/pkgconfig $PKG_CONFIG_PATH"
-else if -x pacman
+else if test -x pacman
   set -g -x PHP_BUILD_CONFIGURE_OPTS "--enable-intl --with-pear"
   set -g -x PHP_BUILD_EXTRA_MAKE_ARGUMENTS -j12
 end
