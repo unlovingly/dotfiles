bff241c6749627277328aece2257fcc6b9a12240
u8y <mail.address.is.unsteady@gmail.com>
2020-12-06 06:39:56 +0900

    fixed duplicate selection issue

--- fish.d/functions/select_repository.fish
+++ fish.d/functions/select_repository.fish
@@ -2,8 +2,8 @@ function select_repository --description "Change current directory to repository
     set -l names (ghq list --unique)
 
     ghq list --unique | \
-    fzf --preview="ghq list --full-path {}" --preview-window=right:80% --select-1 | \
-    read -l line && cd (ghq list --full-path $line | head)
+    fzf --preview="ghq list --full-path {} | head -n 1" --preview-window=right:80% --select-1 | \
+    read -l line && cd (ghq list --full-path $line | head -n 1)
 
     commandline -f repaint
 end
