90cba596b528e4d879ea2796e8ed7924bf382e1e
u8y <mail.address.is.unsteady@gmail.com>
2021-11-28 23:19:04 +0900

    install タスクが失敗していた

--- fish.d/config.fish
+++ fish.d/config.fish
@@ -6,6 +6,5 @@ for file in $config_files
     test -f $XDG_CONFIG_HOME/fish/$file.fish && source $XDG_CONFIG_HOME/fish/$file.fish
 end
 
-status --is-interactive && source (anyenv init - | source)
 starship init fish | source
 zoxide init fish | source
